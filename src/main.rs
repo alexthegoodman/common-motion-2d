88f560ebea743120ff88fbd77b4e9bfb4aeb3c94
Alex Goodman <alexthegoodman@gmail.com>
2025-01-02 17:16:46 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,33 @@ type Elem = f32;
 type Backend = burn::backend::Autodiff<Wgpu>;
 
 fn main() {
-    // let config = ExperimentConfig::new(
-    //     burn::nn::transformer::TransformerEncoderConfig::new(768, 3072, 12, 6)
-    //         .with_norm_first(true)
-    //         .with_dropout(0.1),
-    //     burn::optim::AdamWConfig::new().with_weight_decay(1.0e-8),
-    // );
-
-    // common_motion_2d::training::train::<Backend, MotionDataset>(
-    //     burn::tensor::Device::<Backend>::DiscreteGpu(0),
-    //     MotionDataset::train().expect("Couldn't load training set"),
-    //     MotionDataset::test().expect("Couldn't load test set"),
-    //     config,
-    //     "/tmp/text-generation",
-    // );
-
-    let mut prompts = Vec::new();
-    prompts.push(
-        "0, 5, 361, 161, 305, 217, \n1, 5, 232, 332, 50, 70, \n2, 5, 149, 149, 304, 116, "
-            .to_string(),
+    let config = ExperimentConfig::new(
+        burn::nn::transformer::TransformerEncoderConfig::new(768, 3072, 12, 6)
+            .with_norm_first(true)
+            .with_dropout(0.1),
+        burn::optim::AdamWConfig::new().with_weight_decay(1.0e-8),
     );
 
-    let device = burn::tensor::Device::<Backend>::DiscreteGpu(0);
-
-    infer_from_text::<Backend>(
-        "/tmp/text-generation-e8-rope",
-        &device,
-        prompts,
-        512,
-        1.0, // 1.0 should have no effect
+    common_motion_2d::training::train::<Backend, MotionDataset>(
+        burn::tensor::Device::<Backend>::DiscreteGpu(0),
+        MotionDataset::train().expect("Couldn't load training set"),
+        MotionDataset::test().expect("Couldn't load test set"),
+        config,
+        "/tmp/text-generation",
     );
 
-    // infer_from_text_trainlike::<Backend>("/tmp/text-generation-e8-rope", &device, prompts);
+    // let mut prompts = Vec::new();
+    // prompts.push("305, 217, \n50, 70, \n304, 116, ".to_string());
+
+    // let device = burn::tensor::Device::<Backend>::DiscreteGpu(0);
+
+    // infer_from_text::<Backend>(
+    //     "/tmp/text-generation-e8-rope",
+    //     &device,
+    //     prompts,
+    //     512,
+    //     1.0, // 1.0 should have no effect
+    // );
+
+    // // infer_from_text_trainlike::<Backend>("/tmp/text-generation-e8-rope", &device, prompts);
 }
